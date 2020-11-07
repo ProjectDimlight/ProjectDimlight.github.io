a0a682b0d24f9ebace7cb939abbef3b64dff0b1a
SOL <1846326091@qq.com>
2020-11-07 19:41:09 +0800

    Update TODOlist

--- SOLPage/src/pages/TODOList.vue
+++ SOLPage/src/pages/TODOList.vue
@@ -27,23 +27,43 @@ export default {
       items: [
         {
           name: '面试',
-          detail: '查看邮箱进入面试页面',
-          date: '下周一'
-        },
-        {
-          name: '面试',
-          detail: '查看邮箱进入面试页面',
-          date: '下周五'
+          detail: '查看邮箱进入面试页面，连续两场',
+          date: '下周二'
         },
         {
           name: '无线网络应用',
-          detail: '作业+在线考试',
+          detail: '作业',
           date: '11-9'
         },
         {
-          name: '数字音视频',
-          detail: '转场作业',
+          name: '无线网络应用',
+          detail: '在线考试+实验报告',
+          date: '11-14'
+        },
+        {
+          name: '勇电凯实杯',
+          detail: '方案调研',
+          date: '11-13'
+        },
+        {
+          name: '勇电凯实杯',
+          detail: '具体实现',
           date: '11-20'
+        },
+        {
+          name: '勇电凯实杯',
+          detail: '调优，写报告',
+          date: '11-27'
+        },
+        {
+          name: '棋社策划案',
+          detail: '编写初稿',
+          date: '11-13'
+        },
+        {
+          name: '洛谷教案',
+          detail: '添加例题、作业等具体方案',
+          date: '11-13'
         }
       ]
     }
